22e3a829bd1f4ef0f5a43774ffcae4c8560286fe
Traviteja2 <raviteja199305@gmail.com>
2025-03-20 15:01:25 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,8 +1,20 @@
 package fibonacci;
 
 public class Main {
+	 public static int fib(int n) {
+	        if (n <= 1) return n;
+	        int a = 0, b = 1, c;
+	        for (int i = 2; i <= n; i++) {
+	            c = a + b;
+	            a = b;
+	            b = c;
+	        }
+	        return b;
+	    }
 
 	public static void main(String[] args) {
+		 int n = 10;
+	        System.out.println(fib(n));
 		
 
 	}
